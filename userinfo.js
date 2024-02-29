845789419bd817a3a3d971919a6f9b091c233167
CooperJohnston <coopera1@byu.edu>
2024-02-28 17:26:29 -0700

    login process

--- userinfo.js
+++ userinfo.js
@@ -1,6 +1,14 @@
 function display(){
         const user = document.getElementById("Usa");
         const name = localStorage.getItem("userName");
-        user.textContent = 'Username: ' + name;}
-
+        user.textContent = 'Username: ' + name;
+        const facts = document.createElement("ul");
+        const l = document.createElement('li');
+        l.textContent = "I have given " + Object.keys(JSON.parse(localStorage.getItem("Reviews"))).length + " total ratings";
+        facts.appendChild(l);
+        user.appendChild(facts);}
+function logout(){
+        localStorage.clear();
+        window.location.href = "index.html";
+}
 display();
