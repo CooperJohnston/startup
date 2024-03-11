641d4b6c3efe34f7af45fd24885554a9b0612ad6
CooperJohnston <coopera1@byu.edu>
2024-03-11 00:38:31 -0600

    Moved files private

--- conflictTest.md
+++
@@ -1,4 +0,0 @@
-This is a test to make sure my IDE is allowing proper changes /n
-I do not like Iron Man
-##I love Iron Man
-pooButts
--- login.js
+++
@@ -1,6 +0,0 @@
-function login() {
-  const nameEl = document.querySelector("#User");
-  localStorage.setItem("userName", nameEl.value);
-  window.location.href = "library.html";
-
-}
