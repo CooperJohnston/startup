451d78fb099ac18f7b06760d278c98125aa93e1e
CooperJohnston <coopera1@byu.edu>
2024-03-12 21:08:11 -0600

    fixed 3rd party

--- public/userinfo.js
+++ public/userinfo.js
@@ -1,12 +1,22 @@
 function display(){
+        fetch('https://api.quotable.io/random')
+    .then((response) => response.json())
+    .then((data) => {
         const user = document.getElementById("Usa");
         const name = localStorage.getItem("userName");
         user.textContent = 'Username: ' + name;
+
         const facts = document.createElement("ul");
         const l = document.createElement('li');
+        //saved to local storage at this time because users arent created yet
         l.textContent = "I have given " + Object.keys(JSON.parse(localStorage.getItem("Reviews"))).length + " total ratings";
         facts.appendChild(l);
-        user.appendChild(facts);}
+        const b = document.createElement('li');
+        b.textContent = data.content;
+        facts.appendChild(b);
+        user.appendChild(facts);
+    })}
+
 function logout(){
         localStorage.clear();
         window.location.href = "index.html";
