574b884e8331d0cbe63058b54902f1b4c52382ad
CooperJohnston <coopera1@byu.edu>
2024-03-22 01:17:01 -0600

    log in information

--- public/login.js
+++ public/login.js
@@ -1,6 +1,32 @@
-function login() {
-  const nameEl = document.querySelector("#User");
-  localStorage.setItem("userName", nameEl.value);
-  window.location.href = "library.html";
 
+
+async function loginUser() {
+  loginOrCreate(`/api/auth/login`);
+}
+
+async function createUser() {
+  loginOrCreate(`/api/auth/create`);
+}
+
+async function loginOrCreate(endpoint) {
+  const userName = document.querySelector('#User')?.value;
+  const password = document.querySelector('#psw')?.value;
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify({ email: userName, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  if (response.ok) {
+    localStorage.setItem('userName', userName);
+    window.location.href = 'library.html';
+  } else {
+    const body = await response.json();
+    const modalEl = document.querySelector('#msgModal');
+    modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+    const msgModal = new bootstrap.Modal(modalEl, {});
+    msgModal.show();
+  }
 }
